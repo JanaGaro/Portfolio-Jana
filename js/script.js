9c6ec6d2968197e6df8dc94c3f71a2956bb71112
Garo Jana Mara <garoj1@bfh.ch>
2024-11-29 13:25:55 +0100

    ergäzung Action item -> mailto: janamara.garo@gmail.com

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// htmlcss progress circular bar 
+// htmlcss progress circular bar
 let htmlProgress = document.querySelector(".html-css"),
   htmlValue = document.querySelector(".html-progress");
 
@@ -19,7 +19,7 @@ let progresshtml = setInterval(() => {
   }
 }, htmlspeed);
 
-// javasript progress circular bar 
+// javasript progress circular bar
 let javascriptProgress = document.querySelector(".javascript"),
   javascriptValue = document.querySelector(".javascript-progress");
 
@@ -40,7 +40,7 @@ let progressjs = setInterval(() => {
   }
 }, jsspeed);
 
-// php progress circular bar 
+// php progress circular bar
 let phpProgress = document.querySelector(".php"),
   phpValue = document.querySelector(".php-progress");
 
@@ -61,7 +61,7 @@ let progressphp = setInterval(() => {
   }
 }, phpspeed);
 
-// reactjs progress circular bar 
+// reactjs progress circular bar
 let reactProgress = document.querySelector(".reactjs"),
   reactValue = document.querySelector(".reactjs-progress");
 
@@ -82,7 +82,6 @@ let progressreact = setInterval(() => {
   }
 }, rjsspeed);
 
-
 // filter using javascript
 $(document).ready(function () {
   $(".filter-item").click(function () {
@@ -100,25 +99,23 @@ $(document).ready(function () {
   });
 });
 
-
 // javascript for sticky navbar even if u scroll the navbar will be fixed
-document.addEventListener("DOMContentLoaded", function(){
-  window.addEventListener('scroll', function() {
-      if (window.scrollY > 50) {
-        document.getElementById('navbar-top').classList.add('fixed-top');
-        // add padding top to show content behind navbar
-        navbar_height = document.querySelector('.navbar').offsetHeight;
-        document.body.style.paddingTop = navbar_height + 'px';
-      } else {
-        document.getElementById('navbar-top').classList.remove('fixed-top');
-         // remove padding top from body
-        document.body.style.paddingTop = '0';
-      } 
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 50) {
+      document.getElementById("navbar-top").classList.add("fixed-top");
+      // add padding top to show content behind navbar
+      navbar_height = document.querySelector(".navbar").offsetHeight;
+      document.body.style.paddingTop = navbar_height + "px";
+    } else {
+      document.getElementById("navbar-top").classList.remove("fixed-top");
+      // remove padding top from body
+      document.body.style.paddingTop = "0";
+    }
   });
-}); 
+});
 
-
-// adding funtionality to back to top button 
+// adding funtionality to back to top button
 
 //Get the button
 let mybutton = document.getElementById("btn-back-to-top");
@@ -128,17 +125,14 @@ window.onscroll = function () {
   scrollFunction();
 };
 function scrollFunction() {
-  if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
-  ) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
 }
 // When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click",function(){
+mybutton.addEventListener("click", function () {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 });
